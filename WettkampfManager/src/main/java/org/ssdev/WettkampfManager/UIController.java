de82f5efdd8b15009c28e1c495accd90a1239937
Christian Holler (:decoder) <choller@mozilla.com>
2016-09-28 16:37:15 +0200

    Move Main.fxml and add more menu items

--- WettkampfManager/src/main/java/org/ssdev/WettkampfManager/UIController.java
+++ WettkampfManager/src/main/java/org/ssdev/WettkampfManager/UIController.java
@@ -14,6 +14,7 @@ import javafx.event.EventHandler;
 import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextInputDialog;
@@ -59,6 +60,18 @@ public class UIController {
 	
 	@FXML
 	private Label lastTime;
+	
+	@FXML
+	private MenuItem exportMenuItem;
+	
+	@FXML
+	private MenuItem nameImportMenuItem;
+	
+	@FXML
+	private MenuItem nameExportMenuItem;
+	
+	@FXML
+	private MenuItem closeMenuItem;
 	
     private Main myMain;
     
